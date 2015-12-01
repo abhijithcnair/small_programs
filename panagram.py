44083675efda379f50d4f9904ee76d0d32d8a027
abhijithcnair <abhijithcnair@yahoo.com>
2015-12-01 22:02:29 +0530

    fixed all problem,in working condition

--- panagram.py
+++ panagram.py
@@ -4,9 +4,11 @@ def panagram(check):
     c=0
     for i in alp:
         if i not in check:
-            print "it is not panagram"
-        else:
-            print "it is a panagram"
+            c=1
+    if c==1:
+        print "it is not panagram"
+    else:
+        print "it is a panagram"
 
 def main():
     print "enter a string:"
